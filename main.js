6aaa20f429491c7072fa9e4903e44e3d259f45a3
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2022-08-27 10:54:23 -0500

    fills array, displays to board when clicked

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log("hello world");
+const gameBoard = (() => {
+
+    const board = [];
+
+    function _fillBoardArray() {
+        
+        for (let i = 0; i < 9; i++)
+        {
+            board.push("x");
+        }
+    }
+
+    function _displayMarker(node, array)
+    {
+        node.innerHTML = array;
+    }
+
+    function _fillBoardDiv() {
+        _fillBoardArray();
+        let tiles = document.querySelectorAll(".tile");
+        
+        for (let i = 0; i < board.length; i++){
+            tiles[i].addEventListener("click", () => {
+                console.log(board[i]);
+                tiles[i].innerHTML = board[i];
+                
+            });
+        }
+    };
+    
+    return {
+        
+        fillBoardDiv: function() {
+            _fillBoardDiv();
+        }
+    }
+    
+})();
+
+
+
+let gamePlay = (() => {
+    const turn = (player) => {
+
+    }
+
+})();
+
+gameBoard.fillBoardDiv();
