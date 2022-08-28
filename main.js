b6a831b7f90135ca00e207e9fa86df0b9865ae6b
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2022-08-28 09:44:26 -0500

    Little closer

--- main.js
+++ main.js
@@ -5,22 +5,26 @@ const gameBoard = (() => {
     let selection = "";
 
     function _getSelection() {
+        
         let selections = document.querySelectorAll("button");
         selections.forEach(button => button.addEventListener("click", () => {
         selection = button.id;
         console.log(selection);
+        gamePlay.pOne.selection = selection;
         
         }));    
     }
 
     function _fillBoardDiv() {
         let tiles = document.querySelectorAll(".tile");
-        
+
         for (let i = 0; i < 9; i++){
             tiles[i].addEventListener("click", () => {
                 
-                tiles[i].innerHTML = selection;
-
+                if(gamePlay.pOne.active){
+                    tiles[i].innerHTML = gamePlay.pOne.selection;
+                }
+                
                 board[i] = tiles[i].innerHTML;
             });
         }
@@ -41,12 +45,26 @@ const gameBoard = (() => {
     
 })();
 
-const player = () => {
+const player = (selection, active) => {
 
-    const selection = gameBoard.selection
-    return {selection};
-       
+    selection = gameBoard.selection;
+    active = true;
+
+    return {selection, active};   
 };
-gameBoard.getSelection();
+
+
+
+const gamePlay = (() => {
+    let pTwo = "";
+    gameBoard.getSelection();
+    let pOne = player(gameBoard.selection); 
+
+    return {pOne};
+
+})();
+
+
+
 gameBoard.fillBoardDiv();
 
